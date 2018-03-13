35278bcfd0daad2ea4c69aeb6a284b2e75a77d18
andonary <ando.nary@gmail.com>
2018-03-13 17:34:00 +0100

    Ajout des formulaires + vérification jQuery des données saisies

--- reservation.js
+++ reservation.js
@@ -1,3 +1,17 @@
+function testSuccess(targetId) {
+  $(targetId).css({
+    "border-color": "green",
+    color: "green"
+  });
+}
+
+function testError(targetId) {
+  $(targetId).css({
+    "border-color": "red",
+    color: "red"
+  });
+}
+
 $(document).ready(function() {
   $("div.btnReserv")
     .find("a")
@@ -26,4 +40,46 @@ $(document).ready(function() {
         }
       });
     });
+
+  $("select#nombreDePersonnes")
+    .find("option.nombreSpecial")
+    .click(function() {
+      $("form")
+        .find("div.reservation__SpecialSeptOuPlus")
+        .removeClass("reservation__SpecialSeptOuPlus--hidden");
+    });
+
+  $("select#nombreDePersonnes")
+    .find("option.nombreNormal")
+    .click(function() {
+      $("form")
+        .find("div.reservation__SpecialSeptOuPlus")
+        .addClass("reservation__SpecialSeptOuPlus--hidden");
+    });
+
+  $("textarea#votreMessage").blur(function() {
+    const separateurMotsRegex = /[ '-]+/;
+    let messageUtilisateur = $(this).val();
+    let chaine = messageUtilisateur.split(separateurMotsRegex);
+    let nbWords = chaine.length;
+    0 > nbWords && nbWords <= 100 ? testSuccess(this) : testError(this);
+  });
+
+  $("input#votreNom").blur(function() {
+    const verifRegex = /^[a-z -]+$/i;
+    let nomUtilisateur = $(this).val();
+    nomUtilisateur.match(verifRegex) ? testSuccess(this) : testError(this);
+  });
+
+  $("input#Tel").blur(function() {
+    const verifRegex = /^[0-9 -.()]+$/;
+    let telUtilisateur = $(this).val();
+    telUtilisateur.match(verifRegex) ? testSuccess(this) : testError(this);
+  });
+
+  $("input#SpecialSeptOuPlus").blur(function() {
+    const verifRegex = /^[0-9]{1,2}$/;
+    let SpecialSeptOuPlus = $(this).val();
+    SpecialSeptOuPlus.match(verifRegex) ? testSuccess(this) : testError(this);
+  });
 });

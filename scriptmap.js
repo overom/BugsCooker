3e85cd6535f4336e48d6327598d2cd4a2e63c593
Roxane451 <roxane451@gmail.com>
2018-03-15 13:09:34 +0100

    ajout de la maps

--- scriptmap.js
+++ scriptmap.js
@@ -1,32 +1,110 @@
-var map;
-$(document).ready(function() {
-  map = new GMaps({
-    el: "#map",
-    lat: 45.760255,
-    lng: 4.828231899999992
+function initMap() {
+  let lyon = { lat: 45.760255, lng: 4.828231899999992 };
+  let map = new google.maps.Map(document.getElementById("map-canvas"), {
+    zoom: 16,
+    center: lyon,
+    styles: [
+      {
+        featureType: "landscape.natural",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            visibility: "on"
+          },
+          {
+            color: "#e0efef"
+          }
+        ]
+      },
+      {
+        featureType: "poi",
+        elementType: "geometry.fill",
+        stylers: [
+          {
+            visibility: "on"
+          },
+          {
+            hue: "#1900ff"
+          },
+          {
+            color: "#c0e8e8"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            lightness: 100
+          },
+          {
+            visibility: "simplified"
+          }
+        ]
+      },
+      {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [
+          {
+            visibility: "off"
+          }
+        ]
+      },
+      {
+        featureType: "transit.line",
+        elementType: "geometry",
+        stylers: [
+          {
+            visibility: "on"
+          },
+          {
+            lightness: 700
+          }
+        ]
+      },
+      {
+        featureType: "water",
+        elementType: "all",
+        stylers: [
+          {
+            color: "#7dcdcd"
+          }
+        ]
+      }
+    ]
   });
-  map.addMarker({
-    lat: 45.760255,
-    lng: 4.828231899999992,
-    title: "Lyon",
-    details: {
-      database_id: 42,
-      author: "HPNeo"
-    },
-    click: function(e) {
-      if (console.log) console.log(e);
-      alert("Bug Cooker - 33 Quai Romain Rolland - 69005 Lyon");
-    },
-    mouseover: function(e) {
-      if (console.log) console.log(e);
-    }
+
+  var contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h2 id="firstHeading" class="firstHeading">Restaurant Bug Cooker</h2>' +
+    '<div id="bodyContent">' +
+    "<p><strong>33 Quai Romain Roland 69005 LYON</br>06 79 83 45 15</strong></p>" +
+    "<p><b>Bug cooker</b>, Quand les insectes s’invitent dans la cuisine du chef étoilé <b>David Faure</b>... Prêts pour l’expérience ? " +
+    '<a href="https://bug-cooker.com">' +
+    "</br>https://bug-cooker.com</a>" +
+    "</div>" +
+    "</div>";
+
+  let infowindow = new google.maps.InfoWindow({
+    content: contentString
   });
-  map.addMarker({
-    lat: 45.760255,
-    lng: 4.828231899999992,
-    title: "Marker with InfoWindow",
-    infoWindow: {
-      content: "<p>HTML Content</p>"
-    }
+
+  let marker = new google.maps.Marker({
+    position: lyon,
+    map: map,
+    title: "BugCookers"
   });
-});
+  marker.addListener("mouseover", function() {
+    infowindow.open(map, marker);
+  });
+
+  google.maps.event.addDomListener(window, "resize", function() {
+    let center = map.getCenter();
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(center);
+  });
+}
